a0120aab3be17f3ae666450158f113e056fa3a61
Andrei Ciortea <andrei.ciortea@gmail.com>
2013-09-05 18:39:05 +0200

    [add] fixes

--- client/js/strategy.js
+++ client/js/strategy.js
@@ -4,19 +4,35 @@ function() {
 	var defaultSpeed = 400;
 	var decreaseRate = 0.7;
 
+	// Minimum obstacle distance.
+	var obstacleThreshold = 10;
+
 	var processState = function(state) {
 		
-		switch(line) {
-			case -1 : state.motors[0] *= decreaseRate;
-			case 0 : {
-					state.motors[0] = defaultSpeed;
-					state.motors[1] = defaultSpeed;
-				}
-			case 1 : state.motors[1] *= decreaseRate;
+		if (state.obstacle < obstacleThreshold) {
+			state = processObstacle(state);
+		} else {
+			state = processLine(state);
 		}
-
+		
 		return state;
 	};
+
+	var processObstacle = function(state) {
+		return state;
+	}
+
+	var processLine = function(state) {
+		switch(state.line) {
+			case -1 : state.motors[0] *= decreaseRate; break;
+			case 0 : state.motors[0] = defaultSpeed;
+				 state.motors[1] = defaultSpeed;
+				 break;
+			case 1 : state.motors[1] *= decreaseRate; break;
+		}
+		
+		return state;
+	}
 	
 	return {processState: processState};
 });
